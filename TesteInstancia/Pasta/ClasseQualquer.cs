43c331e7a872fcdb4059eb466afa31fadaa4a119
Carlos Júnior <carlos.roberto.98jr@gmail.com>
2026-01-14 07:50:55 -0300

    feat: teste de instância de classe a partir de nome

---
+++ TesteInstancia/Pasta/ClasseQualquer.cs
@@ -0,0 +1,9 @@
+﻿namespace TesteInstancia.Pasta;
+
+public class ClasseQualquer
+{
+    public void Run()
+    {
+        Console.WriteLine("Teste!");
+    }
+}
---
+++ TesteInstancia/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using TesteInstancia.Pasta;
+
+var assembly = Assembly.GetExecutingAssembly();
+var type = assembly.GetType("TesteInstancia.Pasta.ClasseQualquer");
+var instance = (ClasseQualquer)Activator.CreateInstance(type);
+instance?.Run();
+
+Console.WriteLine("Fim");
